17ca37ffbdfe9a5f8707e398e4b42a678119690d
Nakagawa Kosuke <kosukenakagawa730@gmail.com>
2023-08-11 05:49:57 +0900

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by Nakagawa Kosuke on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaxCalculatorUITests/TaxCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TaxCalculatorUITestsLaunchTests.swift
+//  TaxCalculatorUITests
+//
+//  Created by Nakagawa Kosuke on 2023/08/11.
+//
+
+import XCTest
+
+final class TaxCalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
